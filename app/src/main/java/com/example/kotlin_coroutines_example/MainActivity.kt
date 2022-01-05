d5533547609d784cbe2b0b3c515514211217f907
abhishekdharmik <abhidharmik1997@gmail.com>
2022-01-06 03:08:32 +0530

    init-setup

---
+++ app/src/main/java/com/example/kotlin_coroutines_example/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.kotlin_coroutines_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            setNewText("Click!")
+
+            //IO, Main and Default
+            /*
+             * IO -> Network and Local Database interaction
+             * Main -> Working on Main Thread
+             * Default -> For Large Computational work
+             *
+             */
+
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
+        }
+
+    }
+
+    private fun setNewText(input: String){
+        val newText = text.text.toString() + "\n$input"
+        text.text = newText
+    }
+    private suspend fun setTextOnMainThread(input: String) {
+        withContext (Dispatchers.Main) {
+            setNewText(input)
+        }
+    }
+
+    private suspend fun fakeApiRequest() {
+        logThread("fakeApiRequest")
+
+        val result1 = getResult1FromApi() // wait until job is done
+
+        if ( result1.equals("Result #1")) {
+
+            setTextOnMainThread("Got $result1")
+
+            val result2 = getResult2FromApi() // wait until job is done
+
+            if (result2.equals("Result #2")) {
+                setTextOnMainThread("Got $result2")
+            } else {
+                setTextOnMainThread("Couldn't get Result #2")
+            }
+        } else {
+            setTextOnMainThread("Couldn't get Result #1")
+        }
+    }
+
+    private suspend fun getResult1FromApi(): String {
+        logThread("getResult1FromApi")
+        delay(1000) // Does not block thread. Just suspends the coroutine inside the thread
+        return "Result #1"
+    }
+
+    private suspend fun getResult2FromApi(): String {
+        logThread("getResult2FromApi")
+        delay(1000)
+        return "Result #2"
+    }
+
+    private fun logThread(methodName: String){
+        println("debug: ${methodName}: ${Thread.currentThread().name}")
+    }
+
+}
